876841524759579f5bedf2da651739af9327ebb1
Quentin <pre.quentin+github@gmail.com>
2021-09-29 17:23:50 +0200

    :)

--- modules/config/main.tf
+++ modules/config/main.tf
@@ -6,7 +6,7 @@ locals {
 output "config_dir" { value = local.config_dir }
 output "lambdas" { value = lookup(var.config_file, "lambdas", {}) }
 output "static_files_archive_name" { value = lookup(var.config_file, "staticFilesArchive", "") }
-output "static_files_archive" { value = "${local.config_dir}/${local.static_files_archive_name}" }
+output "static_files_archive" { value = "${local.config_dir}/${lookup(var.config_file, "staticFilesArchive", "")}" }
 output "config_file_images" { value = lookup(var.config_file, "images", {}) }
 output "config_file_version" { value = lookup(var.config_file, "version", 0) }
 output "static_routes_json" { value = lookup(var.config_file, "staticRoutes", []) }
